88e7d785cd1bedea20f339ffb2c2a7a8fcb49043
Mikael Meulle <mikael.meulle@leadingfellows.com>
2019-02-12 11:53:23 +0100

    added custom JS

---
+++ custom/js/custom.js
@@ -0,0 +1,8 @@
+//inlined code
+console.log("custom javascript code aded to reveal.js");
+
+
+$( document ).ready(function() {
+    console.log("set document title");
+    $(this).attr("title", "Reveal.js starterkit TITLE");
+});
